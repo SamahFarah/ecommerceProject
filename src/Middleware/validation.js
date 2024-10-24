8debd3da2a32b0891a8aca8e42fa34df5f240f30
SamahFarah <samahfarah79@gmail.com>
2024-10-24 18:44:08 +0300

    edit on validation middleware

--- src/Middleware/validation.js
+++ src/Middleware/validation.js
@@ -16,29 +16,43 @@ export const generalFeilds={
 
 const validation = (schema) => {
   return (req, res, next) => {
-    const errorMessage=[];
-    let filterData= {...req.body,...req.params,...req.query};
+    const errorMessage = [];
+    
+    
+    let filterData = { ...req.body, ...req.params, ...req.query };
     if(req.file){
       filterData.image=req.file;
     }
-    if (req.files.mainImage) {
-      filterData.mainImage = req.files.mainImage[0]; 
+    // التعامل مع رفع الصور من خلال fields
+    if (req.files) {
+      // إذا كانت mainImage موجودة
+      if (req.files.mainImage && req.files.mainImage.length > 0) {
+        filterData.mainImage = req.files.mainImage[0]; 
+      } else {
+      
+        return res.status(400).json({ message: "mainImage is required" });
+      }
+      
+      // التعامل مع subImages
+      if (req.files.subImages) {
+        filterData.subImages = req.files.subImages; 
+      }
     }
+
     
-    if (req.files.subImages) {
-      filterData.subImages = req.files.subImages; 
+    const { error } = schema.validate(filterData, { abortEarly: false });
+    
+    if (error) {
+      error.details.forEach(err => {
+        const key = err.context.key;
+        errorMessage.push({ [key]: err.message });
+      });
+      return res.status(400).json({ message: "validation error", errors: errorMessage });
     }
-    const {error}=schema.validate(filterData,{abortEarly:false});
-    if(error){
-      error.details.forEach( err=>{
-        const key= err.context.key;
-        errorMessage.push({[key]:err.message})
-      })
-      return res.status(400).json({message:"validation error",errors:errorMessage});
-    }
+
     next();
-  
-}
-}
+  };
+};
+
 
 export default validation;
